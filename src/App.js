aee7aebc6f63951569831307b2d0a9c98f1ac425
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-15 16:37:45 -0300

    About section add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutUs from './components/AboutUs/AboutUs';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="Holder">
       <Header />
+      <AboutUs />
     </div>
   );
 }
