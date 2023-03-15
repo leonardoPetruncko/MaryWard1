ba0e678ee949fb6b542fb5c281f7839f839a1bce
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-15 17:30:37 -0300

    Section Stats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import Header from './components/Header/Header';
+import Stats from './components/Stats/Stats';
 
 function App() {
   return (
     <div className="Holder">
       <Header />
       <AboutUs />
+      <Stats />
     </div>
   );
 }
--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -8,7 +8,6 @@ const AboutUs = () => (
             <div className='about__content text__center'>
                 <h2 className='section__title text__cap'>Sobre</h2>
                 <p className='para__text text__grey'>Um espaço silencioso, com uma Exuberante Natureza. Conta com sala de palestras, sala de apoio, salas de atendimento, Capela Santíssima Trindade (com capacidade para cem pessoas), capela interna com espaço propício para oração, um aconchegante jardim interno, um amplo refeitório (para atender 80 pessoas simultaneamente), quarenta quartos com duas camas e banheiro, sete apartamentos, quarto para grupo, cozinha funcional, arejada e muito bem equipada, quiosques com churrasqueira, terraço para atividades diversas e quadra.</p>
-                {/* <img src = {images.signature} alt = "" /> */}
             </div>
         </div>
     </div>
