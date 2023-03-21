0c980d728e0f04007a3c2ae252d7ebb26ecb48b8
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-21 01:30:05 -0300

    Contact & target no video

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,12 +8,12 @@ const Contact = () => {
     return (
         <div className="contact section__padding bg__whitesmoke">
             <div className="container">
-                <h2 className="section__title text__center">Contato</h2>
-                <p className="para__text text__center">Mande uma mensagem e agende uma Visita!</p>
+                <h2 className="section__title text__center fw__2">Contato</h2>
+                <p className="para__text text__center fw__3">Mande uma mensagem e agende uma Visita!</p>
 
                 <div className="contact__content">
                     <form>
-                        <a className="btn btn__blue" href="https://web.whatsapp.com/">send message</a>
+                        <a className="btn btn__blue" target="_blank" href="https://api.whatsapp.com/send?phone=5511976774420&text=Ol%C3%A1,%20Gostaria%20de%20agendar%20uma%20visita!">Whats</a>
                     </form>
                 </div>
             </div>
