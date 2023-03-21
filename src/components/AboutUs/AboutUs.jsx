8b4ccc75d13ed4361a1ccdb97c686b136bf85d3a
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-21 01:01:19 -0300

    Work e about style

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => (
         <div className='container'>
             <div className='about__content text__center'>
                 <h2 className='section__title text__cap fw__2'>Sobre</h2>
-                <p className='para__text text__grey'>Um espaço silencioso, com uma Exuberante Natureza. Conta com sala de palestras, sala de apoio, salas de atendimento, Capela Santíssima Trindade (com capacidade para cem pessoas), capela interna com espaço propício para oração, um aconchegante jardim interno, um amplo refeitório (para atender 80 pessoas simultaneamente), quarenta quartos com duas camas e banheiro, sete apartamentos, quarto para grupo, cozinha funcional, arejada e muito bem equipada, quiosques com churrasqueira, terraço para atividades diversas e quadra.</p>
+                <p className='para__text text__grey fw__3'>Um espaço silencioso, com uma Exuberante Natureza. Conta com sala de palestras, sala de apoio, salas de atendimento, Capela Santíssima Trindade (com capacidade para cem pessoas), capela interna com espaço propício para oração, um aconchegante jardim interno, um amplo refeitório (para atender 80 pessoas simultaneamente), quarenta quartos com duas camas e banheiro, sete apartamentos, quarto para grupo, cozinha funcional, arejada e muito bem equipada, quiosques com churrasqueira, terraço para atividades diversas e quadra.</p>
             </div>
         </div>
     </div>
--- src/components/WorkProcess/WorkProcess.jsx
+++ src/components/WorkProcess/WorkProcess.jsx
@@ -10,8 +10,8 @@ const WorkProcess = () => {
         <div className="workprocess section__padding">
             <div className="container">
                 <div className="workprocess__content text__center">
-                    <h2 className="section__title text__cap">Venha conhecer</h2>
-                    <p className="para__text"> Um espaço silencioso, com uma Exuberante Natureza. Conta com sala de palestras, sala de apoio, salas de atendimento, Capela Santíssima Trindade (com capacidade para cem pessoas) </p>
+                    <h2 className="section__title text__cap fw__2">Venha conhecer</h2>
+                    <p className="para__text fw__3"> Um espaço silencioso, com uma Exuberante Natureza. Conta com sala de palestras, sala de apoio, salas de atendimento, Capela Santíssima Trindade (com capacidade para cem pessoas) </p>
 
                     <div className="vid__container">
                     <ReactPlayer url='https://youtu.be/ysGvmQloiaA'/>
